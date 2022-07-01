6017898aa469fe5dbeaf79fed2cd9d85304a6c8c
Ross Cooper <37559715+Froskk@users.noreply.github.com>
2022-07-01 17:20:42 +0100

    add (peer)deps

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
   extends: [
-    'prettier',
     'eslint:recommended',
+    'prettier',
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
   ],
